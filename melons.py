86794d24e31c11272a04eaa3a98513fb50185031
suelenk <suelenmatosr@gmail.com>
2022-08-31 00:00:35 -0400

    created logic for the further study

--- melons.py
+++ melons.py
@@ -1,24 +1,11 @@
 """Classes for melon orders."""
 
-
-from os import TMP_MAX
-
-
-class AbstractMelonOrder:      
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5 
-        
-        if self.species == "Christmas":
-            base_price = base_price * 1.5 #making the new price 1.5 times as much as base price
-       
-        total = (1 + self.tax) * self.qty * base_price            
-
-        return total
+import datetime
+import random
 
 
+
+class AbstractMelonOrder:   
 
     def __init__(self, species, qty):
     
@@ -26,14 +13,49 @@ class AbstractMelonOrder:
         self.species = species
         self.qty = qty
         self.shipped = False
+
+        print(qty)
+        if qty > 100:
+            raise TooManyMelonsError("No more than 100 melons!")
+
+
+    def get_base_price(self):
+
+        base_price = random.randint(5,9)
+
+        current_time = datetime.datetime.now() #getting the live hour
+
+        week_day = current_time.weekday() #getting the current day
+        current_hour = current_time.hour #getting the current hour
+        # current_hour = 9 # testing with rush hour
+
+        #checking if day and time is in between the rush hour 
+        if week_day <= 4:
+
+            if current_hour >= 8 and current_hour <= 11:
+                base_price = base_price + 4 #incresing the price if it is rush hour
+
+        # print(generate_random_num) test
+        return base_price
+
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = self.get_base_price()
+        
+        if self.species == "Christmas":
+            base_price = base_price * 1.5 #making the new price 1.5 times as much as base price
+       
+        total = (1 + self.tax) * self.qty * base_price            
+
+        return total   
        
 
     def mark_shipped(self):
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
-
-
 
 
 
@@ -67,6 +89,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
         super().__init__(species, qty)
         self.country_code = country_code
 
+
     def get_total(self):
 
         total_including_flat_fee = super().get_total()
@@ -92,10 +115,13 @@ class GovernmentMelonOrder(AbstractMelonOrder):
 
         self.passed_inspection = passed
 
-    def __init__(self, species, qty):
+class TooManyMelonsError(ValueError):
+    pass
 
-        super().__init__(species, qty)
 
 
-# melon_gov_order = InternationalMelonOrder("ma", 9, 512)
-# print(melon_gov_order.get_total())
+
+# print(current_time.weekday())
+# print(current_time.hour)
+melon_gov_order = DomesticMelonOrder(9, 512)
+print(melon_gov_order.get_total())
